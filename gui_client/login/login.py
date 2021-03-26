c8c8a73ee84c7b557ab2e881d38f3b4edfb77e21
Aud <aud@insights.gg>
2021-03-25 19:47:50 -0500

    Basis for token generation was added to UI along with error handling

--- gui_client/login/login.py
+++ gui_client/login/login.py
@@ -9,11 +9,29 @@ from dearpygui import core, simple
 
 #Setup login call back
 def request_login():
-    #Simulate a request check
-    console_client.client.test()
+    #Get info from login input
+    login_key = core.get_value("login_input")
 
-    #Now finish 
-    login_succeeded()
+    #Check login key
+    login_attempt = console_client.client.save_auth_token(login_key)
+
+    #If the login succeeded
+    if login_attempt.success:
+        login_succeeded()
+
+    #If login key failed, say something
+    else:
+        if core.does_item_exist("login_error_text"):
+            core.delete_item("login_error_text")
+
+        core.add_text(
+            "login_error_text", 
+            default_value="There was an error checking your key", 
+            before="login_button",
+            wrap=0
+        )
+
+
 
 
 def login_succeeded():
@@ -30,6 +48,9 @@ def create_login():
         core.add_text("LOGIN")
 
         core.add_input_text("login_input", hint="auth key", password=True, label="")
+        core.add_text("login_error_text", default_value=" ", before="login_button")
 
-        core.add_button("login_button", callback=request_login)
+
+
+        core.add_button("login_button", label="Login Button", callback=request_login)
 
